fb83f02c4852b622eb537521a0d1fb1dbe1b26e1
turbogous <turbogus85@gmail.com>
2014-03-23 22:13:00 +0100

    Mis de l'ordre dans le dossier du mod.

---
+++ init.lua
@@ -0,0 +1,36 @@
+--[[
+
+Eboulement 
+
+Créé par turbogus. Licence GPL2 ou supérieur.
+
+Simule la mort dans un éboulement
+
+]]--
+
+
+monMessage = "Vous etes enterré ! "
+
+minetest.register_on_punchnode(function(p, node, player)
+	local nomDuJoueur = player:get_player_name()
+	
+	local pos = player:getpos()
+		
+	pos.x = (pos.x+0)
+	pos.y = (pos.y+0)
+	pos.z = (pos.z+0)
+	
+	n_head = minetest.env:get_node(pos).name
+	
+	
+	if n_head=="default:gravel" or n_head=="default:dirt" or n_head=="default:sand" or n_head=="default:desert_sand"  or n_head=="default:stone" or n_head=="default:desert_stone" or n_head=="default:stone_with_coal" or n_head=="default:stone_with_iron" or n_head=="default:stone_with_copper" or n_head=="default:stone_with_mese" or n_head=="default:stone_with_gold" or n_head=="default:stone_with_diamond" then
+	
+		minetest.chat_send_player(nomDuJoueur, monMessage)
+		
+		player:set_hp(player:get_hp()-1)
+	end
+	
+	
+	
+end)
+
