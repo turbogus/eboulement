8aae59aa5b2a5bf0703f40727f9c8310c7db5cad
turbogous <turbogus85@gmail.com>
2014-03-23 22:25:39 +0100

    Correction.
    Passé la valeur Y a 0.5 ( sinon on prend les pieds du joueur !!! ).
    Séparation des matériaux "friables" ( on perd doucement de la vie), et des matériaux
    durs ( on perd sa vie d'un coup ).

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ Simule la mort dans un éboulement
 ]]--
 
 
-monMessage = "Vous etes enterré ! "
+monMessage = "Vous etes sous terre ! "
 
 minetest.register_on_punchnode(function(p, node, player)
 	local nomDuJoueur = player:get_player_name()
@@ -17,20 +17,25 @@ minetest.register_on_punchnode(function(p, node, player)
 	local pos = player:getpos()
 		
 	pos.x = (pos.x+0)
-	pos.y = (pos.y+0)
+	pos.y = (pos.y+0.5)
 	pos.z = (pos.z+0)
 	
 	n_head = minetest.env:get_node(pos).name
 	
 	
-	if n_head=="default:gravel" or n_head=="default:dirt" or n_head=="default:sand" or n_head=="default:desert_sand"  or n_head=="default:stone" or n_head=="default:desert_stone" or n_head=="default:stone_with_coal" or n_head=="default:stone_with_iron" or n_head=="default:stone_with_copper" or n_head=="default:stone_with_mese" or n_head=="default:stone_with_gold" or n_head=="default:stone_with_diamond" then
+	if n_head=="default:gravel" or n_head=="default:dirt" or n_head=="default:sand" or n_head=="default:desert_sand" then
 	
 		minetest.chat_send_player(nomDuJoueur, monMessage)
 		
 		player:set_hp(player:get_hp()-1)
 	end
 	
+	if n_head=="default:stone" or n_head=="default:desert_stone" or n_head=="default:stone_with_coal" or n_head=="default:stone_with_iron" or n_head=="default:stone_with_copper" or n_head=="default:stone_with_mese" or n_head=="default:stone_with_gold" or n_head=="default:stone_with_diamond" then
 	
+		minetest.chat_send_player(nomDuJoueur, monMessage)
+		
+		player:set_hp(0)
+	end
 	
 end)
 
